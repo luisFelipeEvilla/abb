29334be50f336caa66b7c7db6be1aa51a43140c1
Juan Pablo <juanpabloprada02112000>
2020-08-30 20:47:28 -0500

    PRUEBALO

--- src/arbolbinario/Arbol.java
+++ src/arbolbinario/Arbol.java
@@ -5,6 +5,8 @@
  */
 package arbolbinario;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 import javax.swing.JPanel;
 import utils.ArbolGrafico;
@@ -14,12 +16,14 @@ import utils.ArbolGrafico;
  * @author JPPM
  */
 public class Arbol {
-    
+
     NodoArbol raiz;
+
     //inicializo en raíz en nulo arbol vacío
     public Arbol() {
         raiz = null;
     }
+
     //inserta nodo
     public void addNodo(double numero) {
         NodoArbol nuevoNodo = new NodoArbol(numero);
@@ -27,8 +31,7 @@ public class Arbol {
         // la raíz sera el NuevoNodo
         if (raiz == null) {
             raiz = nuevoNodo;
-        }
-        //no está vacío
+        } //no está vacío
         //lo que hara es ir buscando donde insertar el NuevoNodo
         else {
             //Utilizar un apuntador que lleve la referencia del nodo padre a NuevoNodo
@@ -47,22 +50,19 @@ public class Arbol {
                         padre.hijoIzquierdo = nuevoNodo;
                         return;
 
-                    } 
-                    //se va moviendo el padre
+                    } //se va moviendo el padre
                     else {
-                        
+
                         padre = aux;
-                    }   
-                }
-                // si el NuevoNodo tiene un numero mayor se mueve al hijo Derecha aux
+                    }
+                } // si el NuevoNodo tiene un numero mayor se mueve al hijo Derecha aux
                 else {
                     aux = aux.hijoDerecho;
                     //encontro lugar 
                     if (aux == null) {
                         padre.hijoDerecho = nuevoNodo;
                         return;
-                    }
-                     //se va moviendo el padre
+                    } //se va moviendo el padre
                     else {
                         padre = aux;
                     }
@@ -80,24 +80,27 @@ public class Arbol {
     }
 
     public String iterativeInOrder(NodoArbol raiz) {
+
         String recorrido = "";
         if (raiz != null) {
             Stack<NodoArbol> p = new Stack<NodoArbol>();
             while (true) {
                 if (raiz != null) {
                     p.push(raiz);
-                    raiz=raiz.hijoIzquierdo;
-                }else{
-                    if(p.empty()){
+                    raiz = raiz.hijoIzquierdo;
+                } else {
+                    if (p.empty()) {
                         break;
                     }
-                    raiz=p.pop();
-                    recorrido += raiz.getNumero();
-                    raiz=raiz.hijoDerecho;
+                    raiz = p.pop();
+                    recorrido += raiz.getNumero() + " ,";
+                    raiz = raiz.hijoDerecho;
                 }
             }
         }
-        return recorrido;
+        StringBuffer sb = new StringBuffer(recorrido);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 
     public void recorridoPreOrder(NodoArbol raiz) {
@@ -119,7 +122,7 @@ public class Arbol {
             while (p.empty() == false) {
 
                 raiz = p.pop();
-                recorrido = recorrido + raiz.getNumero() + ", ";
+                recorrido = recorrido + raiz.getNumero() + " ,";
                 if (raiz.hijoDerecho != null) {
                     p.push(raiz.hijoDerecho);
                 }
@@ -128,61 +131,176 @@ public class Arbol {
                 }
             }
         }
-        return recorrido;
+        StringBuffer sb = new StringBuffer(recorrido);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
-    
-    public String recorridoPostOrder(NodoArbol raiz){
+
+    public String recorridoPostOrder(NodoArbol raiz) {
         String recorrido = "";
-        if(raiz==null){
+        if (raiz == null) {
             return "";
-        }else{
-         recorridoPostOrder( raiz.hijoIzquierdo);
-         recorridoPostOrder( raiz.hijoDerecho);
-           recorrido = recorrido + raiz.getNumero();
+        } else {
+            recorridoPostOrder(raiz.hijoIzquierdo);
+            recorridoPostOrder(raiz.hijoDerecho);
+            recorrido = recorrido + raiz.getNumero() + "  ,";
         }
-        return recorrido;
+        StringBuffer sb = new StringBuffer(recorrido);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
-    public String iterativePostOrder(NodoArbol raiz){
+
+    public String iterativePostOrder(NodoArbol raiz) {
         String recorrido = "";
-        if(raiz==null){
+        if (raiz == null) {
             return "";
-        }else{
-             Stack<NodoArbol> p1 = new Stack<NodoArbol>();
-             Stack<NodoArbol> p2 = new Stack<NodoArbol>();
-             p1.push(raiz);
-             while(p1.empty()==false){
-                 raiz=p1.pop();
-                 p2.push(raiz);
-                 if(raiz.hijoIzquierdo!=null){
-                     p1.push(raiz.hijoIzquierdo);
-                 }
-                 if(raiz.hijoDerecho!=null){
-                    p1.push(raiz.hijoDerecho) ;
-                 }
-             }
-             while(p2.empty()==false){
-                 raiz=p2.pop();
-                 recorrido = recorrido + raiz.getNumero() + ", ";
-             }
+        } else {
+            Stack<NodoArbol> p1 = new Stack<NodoArbol>();
+            Stack<NodoArbol> p2 = new Stack<NodoArbol>();
+            p1.push(raiz);
+            while (p1.empty() == false) {
+                raiz = p1.pop();
+                p2.push(raiz);
+                if (raiz.hijoIzquierdo != null) {
+                    p1.push(raiz.hijoIzquierdo);
+                }
+                if (raiz.hijoDerecho != null) {
+                    p1.push(raiz.hijoDerecho);
+                }
+            }
+            while (p2.empty() == false) {
+                raiz = p2.pop();
+                recorrido = recorrido + raiz.getNumero() + "  ,";
+            }
         }
-        return recorrido;
+        StringBuffer sb = new StringBuffer(recorrido);
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
-    
-    public int alturaArbol(NodoArbol raiz){
-        if(raiz==null){
+
+    public String recorridoPorNivel(NodoArbol raiz) {
+        String numero = "";
+        if (raiz == null) {
+            return "";
+        } else {
+            Queue<NodoArbol> cola = new LinkedList<NodoArbol>();
+            cola.add(raiz);
+            while (cola.isEmpty() == false) {
+                raiz = cola.poll();
+                numero = numero + raiz.getNumero() + "  ,";
+                if (raiz.getIzq() != null) {
+                    cola.add(raiz.hijoIzquierdo);
+                }
+                if (raiz.getDer() != null) {
+                    cola.add(raiz.hijoDerecho);
+                }
+            }
+            StringBuffer sb = new StringBuffer(numero);
+            sb.deleteCharAt(sb.length() - 1);
+            return sb.toString();
+        }
+
+    }
+
+    public void eliminarNodo(double numero) {
+        boolean esHijoIzq = true;
+        NodoArbol padre = raiz;
+        NodoArbol auxiliar = raiz;
+        while (auxiliar.getNumero() != numero) {
+            padre = auxiliar;
+            if (numero < auxiliar.getNumero()) {
+                esHijoIzq = true;
+                auxiliar = auxiliar.hijoIzquierdo;
+            } else {
+                esHijoIzq = false;
+                auxiliar = auxiliar.hijoDerecho;
+            }
+            if (auxiliar == null) {
+                return;
+            }
+
+        }
+        if (auxiliar != null) {
+            //Elimina Nodos Hojas
+            if (auxiliar.getIzq() == null && auxiliar.getDer() == null) {
+                if (auxiliar == raiz) {
+                    raiz = null;
+
+                } else if (esHijoIzq) {
+                    padre.hijoIzquierdo = null;
+                } else if (!esHijoIzq) {
+                    padre.hijoDerecho = null;
+                }
+                //Elimina nodos con un solo hijo en este caso el nodo tiene hijo en la izqueirda
+            } else if (auxiliar.getIzq() != null && auxiliar.getDer() == null) {
+                if (auxiliar == raiz) {
+                    raiz = padre.hijoIzquierdo;
+                } else if (esHijoIzq) {
+                    padre.hijoIzquierdo = auxiliar.hijoIzquierdo;
+                } else if (!esHijoIzq) {
+                    padre.hijoDerecho = auxiliar.hijoIzquierdo;
+                }
+                //Elimina nodos con un solo hijo en este caso el nodo tiene hijo en la izqueirda
+            } else if (auxiliar.getIzq() == null && auxiliar.getDer() != null) {
+                if (auxiliar == raiz) {
+                    raiz=padre.hijoDerecho;
+                } else if (esHijoIzq) {
+                    padre.hijoIzquierdo = auxiliar.hijoDerecho;
+                } else if (!esHijoIzq) {
+                    padre.hijoDerecho = auxiliar.hijoDerecho;
+                }
+                //Elimina nodos con dos hijos
+            }else{
+                NodoArbol replace = getReplace(auxiliar);
+                 if (auxiliar == raiz) {
+                    raiz=replace;
+                } else if (esHijoIzq) {
+                    padre.hijoIzquierdo = replace;
+                } else if (!esHijoIzq) {
+                    padre.hijoDerecho = replace;
+                }
+                 replace.hijoIzquierdo=auxiliar.hijoIzquierdo;
+            
+            }
+            
+
+        }
+        System.out.println(auxiliar.getNumero());
+
+    }
+        public NodoArbol getReplace(NodoArbol nodeReem) {
+        NodoArbol reemplazarPadre = nodeReem;
+        NodoArbol reemplazo=nodeReem;
+        NodoArbol auxiliar=nodeReem.hijoDerecho; 
+        while(auxiliar!=null){
+            reemplazarPadre=reemplazo;
+            reemplazo=auxiliar;
+            auxiliar=auxiliar.hijoIzquierdo;
+        }
+        if(reemplazo!=nodeReem.hijoDerecho){
+            reemplazarPadre.hijoIzquierdo=reemplazo.hijoDerecho;
+            reemplazo.hijoDerecho=nodeReem.hijoDerecho;
+        }
+        return reemplazo;
+    }
+
+    public int alturaArbol(NodoArbol raiz) {
+        if (raiz == null) {
             return -1;
-        }else{
-            int alturaSubArbolIzq=alturaArbol(raiz.hijoIzquierdo);
-            int alturaSubArbolDer=alturaArbol(raiz.hijoDerecho);
-            return 1+Math.max(alturaSubArbolDer, alturaSubArbolIzq);
+        } else {
+            int alturaSubArbolIzq = alturaArbol(raiz.hijoIzquierdo);
+            int alturaSubArbolDer = alturaArbol(raiz.hijoDerecho);
+            return 1 + Math.max(alturaSubArbolDer, alturaSubArbolIzq);
         }
     }
 
     public NodoArbol getRaiz() {
         return raiz;
     }
-    
+
     public JPanel getDibujo() {
         return new ArbolGrafico(this);
     }
+
+
 }
