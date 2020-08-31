ce8d0226a7097326f3ca806c16300dc32ff65913
Juan Pablo <juanpabloprada02112000>
2020-08-30 20:50:19 -0500

    PRUEBALO

--- src/arbolbinario/ArboleBinario.java
+++ src/arbolbinario/ArboleBinario.java
@@ -22,7 +22,24 @@ public class ArboleBinario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Home app = new Home();
+//        Home app = new Home();
+          Arbol a = new Arbol ();
+          Scanner tecla = new Scanner (System.in);
+          int c=0;
+          int n;
+          while(c<4){
+              System.out.println("digite n");
+              n=tecla.nextInt();
+              a.addNodo(n);
+              c++;
+          }
+          a.recorridoInOrder(a.getRaiz());
+          System.out.println("\n");
+          System.out.println("digite s");
+          int s=tecla.nextInt();
+          a.eliminarNodo(s);
+          System.out.println("\n");
+          a.recorridoInOrder(a.getRaiz());
     }
 }
 
